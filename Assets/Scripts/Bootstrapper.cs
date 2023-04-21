b0b1eb5ab7b8d7cfb879bbb95e81b472b9313a52
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2023-04-21 16:47:31 +0300

    Add bootstrapper to initialize scope manager.

---
+++ Assets/Scripts/Bootstrapper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public static class Bootstrapper
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Initiailze()
+    {
+        var scopeManagerInstance = ScopeManager.Instance;
+        var applicationScope = scopeManagerInstance.CreateScope(Scope.APPLICATION);
+        Object.DontDestroyOnLoad(applicationScope.ScopeObject);
+        var sceneService = applicationScope.ScopeObject.AddComponent<SceneService>();
+        applicationScope.RegisterService(sceneService);
+    }
+}
