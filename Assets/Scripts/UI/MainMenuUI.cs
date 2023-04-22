8dfa4d7e6ac6ce6e4f7aa85cc9810da06f8487b0
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2023-04-22 19:55:10 +0300

    Move script files.

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -4,12 +4,18 @@ public class MainMenuUI : MonoBehaviour
 {
     public void OnPlayButtonClicked()
     {
-       var sceneService =  ScopeManager.Instance.GetService<SceneService>(Scope.APPLICATION);
-        sceneService.ChangeScene(Scene.GAMEPLAY);
+          var sceneService =  ScopeManager.Instance.GetService<SceneService>(Scope.APPLICATION);
+           sceneService.ChangeScene(Scene.GAMEPLAY);
+        
+        /*
+        var popupService = ScopeManager.Instance.GetService<PopupService>(Scope.APPLICATION);
+        popupService.ShowPopup<QuitWarningPopup>();
+        */
     }
 
     public void OnLeaderboardButtonClicked()
     {
-
+        var popupService = ScopeManager.Instance.GetService<PopupService>(Scope.APPLICATION);
+        popupService.ShowPopup<LeaderboardPopup>();
     }
 }
